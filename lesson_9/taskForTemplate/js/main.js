de0cde3e9aa917815dfa0afc0c9cf69f0361b905
Bohdanqaz <mrs.dozersua@gmail.com>
2023-03-01 16:16:28 +0200

    edit main.js. Complete task homework learn 9 in JS

---
+++ lesson_9/taskForTemplate/js/main.js
@@ -0,0 +1,13 @@
+// - Є масив:
+//     ['Main','Products','About us','Contacts']
+// Взяти файл template1.html та додати в нього скріпт котрий для кожного елементу масиву створює li та додає його до блоку .menu
+// Завдання робити через цикли.
+//
+// let arr = ['Main', 'Products', 'About us', 'Contacts'];
+// let li;
+// let wayForLi;
+// for (const arrItem of arr) {
+//     li = document.createElement('li');
+//     wayForLi = document.getElementsByClassName('menu')[0];
+//     wayForLi.appendChild(li).innerText = `${arrItem}`
+// }
